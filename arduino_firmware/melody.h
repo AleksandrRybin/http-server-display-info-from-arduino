72b80cd43aa6b98fc784ca4931bd3c3196ef0c1d
AleksandrRybin <Vfibyrf007@gmail.com>
2019-06-12 22:57:09 +0300

    melodies in separate inc file

---
+++ arduino_firmware/melody.h
@@ -0,0 +1,13 @@
+#include "pitches.h"
+  
+static const uint16_t init_melody[] = {NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4};
+static const uint8_t init_note_durations[] = {4, 8, 8, 4, 4, 4, 4, 4};
+static const uint8_t init_melody_duration = 8;
+
+static const uint16_t gain_10_melody[] = {NOTE_C1, NOTE_G2, NOTE_G4, NOTE_A4};
+static const uint8_t gain_10_note_durations[] = {4, 8, 8, 4};
+static const uint8_t gain_10_melody_duration = 4;
+
+static const uint16_t gain_100_melody[] = {NOTE_B2, NOTE_F1, NOTE_E5, NOTE_A1};
+static const uint8_t gain_100_note_durations[] = {4, 8, 8, 4};
+static const uint8_t gain_100_melody_duration = 4;
